83462691849b1f2c4728c0f674ab6847cf34c2b7
Fabian Ardila <fabianra97@gmail.com>
2022-01-10 18:43:38 -0500

    :sparkles: Retrieve data from quotes

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,5 +1,9 @@
 import scrapy
 
+# Titulo = //h1/a/text()
+# Citas = //span[@class="text" and @itemprop="text"]/text()
+# top ten tags = //div[contains(@class,"tags-box")]/span[@class="tag-item"]/a/text()
+
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
@@ -8,5 +12,23 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         print('*' * 10)
-        print(response.status)
-        print(response.headers)
+        print('\n\n')
+        
+        title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo: {title}')
+        print('\n\n')
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        print('Citas: ')
+        for quote in quotes:
+            print(f'- {quote}')
+        print('\n\n')
+        
+        top_ten_tags = response.xpath('//div[contains(@class,"tags-box")]/span[@class="tag-item"]/a/text()').getall()
+        print('Top ten tags:')
+        for tag in top_ten_tags:
+            print(f'- {tag}')
+        print('\n\n')
+
+        print('\n\n\n')
+        print('*' * 10)
