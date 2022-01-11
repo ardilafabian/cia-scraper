7cf3d27dd084e53a437ed34a94dc8222c5d4daff
Fabian Ardila <fabianra97@gmail.com>
2022-01-10 19:20:52 -0500

    :sparkles: Add follow feature to follow links

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,12 +3,17 @@ import scrapy
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # top ten tags = //div[contains(@class,"tags-box")]/span[@class="tag-item"]/a/text()
+# next page button = //ul[@class="pager"]/li[@class="next"]/a/@href
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
         'https://quotes.toscrape.com/'
     ]
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
@@ -20,3 +25,7 @@ class QuotesSpider(scrapy.Spider):
             'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
+
+        next_page_button_link = response.xpath('//ul[@class="pager"]/li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse)
