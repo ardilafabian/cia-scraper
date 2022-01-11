483afdaccbba3b918d9bbcba5ade2d41b4ae416e
Fabian Ardila <fabianra97@gmail.com>
2022-01-10 19:58:05 -0500

    :sparkles: Add custom settings to project

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -12,25 +12,35 @@ class QuotesSpider(scrapy.Spider):
     ]
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMITS_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['fabianra97@gmail.com'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'PepitoMartinez',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
             quotes = kwargs['quotes']
+            authors = kwargs['authors']
         quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+        authors.extend(response.xpath('//div[@class="quote"]/span[not(@class)]/small[@class="author"]/text()').getall())
 
         next_page_button_link = response.xpath('//ul[@class="pager"]/li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
         else:
             yield {
-                'quotes': quotes
+                'quotes': quotes,
+                'authors': authors
             }
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        authors = response.xpath('//div[@class="quote"]/span[not(@class)]/small[@class="author"]/text()').getall()
         top_tags = response.xpath('//div[contains(@class,"tags-box")]/span[@class="tag-item"]/a/text()').getall()
 
         top = getattr(self, 'top', None)
@@ -45,4 +55,4 @@ class QuotesSpider(scrapy.Spider):
 
         next_page_button_link = response.xpath('//ul[@class="pager"]/li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
